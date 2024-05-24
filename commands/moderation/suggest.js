921cc79df6d021a12f942ca837d14ca5ac2ab1a9
Awsome-Creeper9 <gherdt@outlook.com>
2024-05-24 14:10:03 -0500

    Changed /suggest to use a modal

--- commands/moderation/suggest.js
+++ commands/moderation/suggest.js
@@ -1,30 +1,69 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, Events, EmbedBuilder, ModalBuilder} = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('suggest')
-		.setDescription('Send a suggestion to Awsome_Creeper9')
-		.addStringOption(option =>
-			option.setName('suggestion')
-				.setDescription('Message sent to Awsome_Creeper9')
-				.setRequired(true)),
+		.setDescription('Send a suggestion to Awsome_Creeper9'),
 	async execute(interaction) {
 		const userId = '564469575240908817'
-		var canSend = true;
+		let canSend = true;
 		const bannedUsers = [
 			''
 		]
-
-		const suggestion = interaction.options.getString('suggestion')
 
 		for (let i = 0; i < bannedUsers.length; i++) {
 			if (interaction.user.id === bannedUsers[i]) {
 				canSend = false
 			}
 		}
-		if (canSend === true) {
-			await interaction.client.users.send(userId, `Suggestion from ${interaction.user}: ${suggestion}`);
-		}
-		await interaction.reply({ content: 'Your suggestion has been sent!', ephemeral: true });
+
+		const placeholderTitles = ['Color space options for /gradient', 'Admin Commands', 'Blacklist Request', 'New Command Idea: /test'];
+
+		const modal = new ModalBuilder()
+			.setCustomId('suggest-modal')
+			.setTitle('Suggestion')
+			.addComponents(
+				new ActionRowBuilder().addComponents(
+					new TextInputBuilder()
+						.setCustomId('title-input')
+						.setLabel('Title')
+						.setStyle(TextInputStyle.Short)
+						.setMaxLength(50)
+						.setPlaceholder(placeholderTitles[Math.floor(Math.random() * (placeholderTitles.length - .1))])
+						.setRequired(true),
+				),
+				new ActionRowBuilder().addComponents(
+					new TextInputBuilder()
+						.setCustomId('description-input')
+						.setLabel('Description')
+						.setStyle(TextInputStyle.Paragraph)
+						.setRequired(true),
+				),
+			);
+
+		await interaction.showModal(modal);
+
+		interaction.client.once(Events.InteractionCreate, async interaction => {
+			if (!interaction.isModalSubmit()) return;
+
+			if (canSend === true) {
+				const suggestion = {
+					title: interaction.fields.getTextInputValue('title-input'),
+					desc: interaction.fields.getTextInputValue('description-input'),
+				}
+				const embed = new EmbedBuilder()
+					.setColor(0xFFFF80)
+					.setTitle('Suggestion:')
+					.setDescription(suggestion.title)
+					.addFields({ name: 'Description:', value: suggestion.desc })
+					.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL() })
+					.setFooter({ text: interaction.user.id })
+					.setTimestamp(Date.now())
+
+				await interaction.client.users.send(userId, { embeds: [embed] });
+			}
+			await interaction.reply({ content: 'Your suggestion has been sent!', ephemeral: true });
+		})
+
 	},
 };
